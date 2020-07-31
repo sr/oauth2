aac2bb920426df8010148824a62288c12e399741
Simon Rozet <me@simonrozet.com>
2020-07-31 12:44:42 +0200

    set scope parameter when refreshing token
    The Concept2 Logbook API requires this:
    https://log.concept2.com/developers/documentation/#authentication-authorization-code-get

--- token.go
+++ token.go
@@ -154,6 +154,10 @@ func tokenFromInternal(t *internal.Token) *Token {
 // This token is then mapped from *internal.Token into an *oauth2.Token which is returned along
 // with an error..
 func retrieveToken(ctx context.Context, c *Config, v url.Values) (*Token, error) {
+	if len(c.Scopes) > 0 {
+		v.Set("scope", strings.Join(c.Scopes, " "))
+	}
+
 	tk, err := internal.RetrieveToken(ctx, c.ClientID, c.ClientSecret, c.Endpoint.TokenURL, v, internal.AuthStyle(c.Endpoint.AuthStyle))
 	if err != nil {
 		if rErr, ok := err.(*internal.RetrieveError); ok {
